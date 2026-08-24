f8460c9ddfdd2b24b40e1e6ffb4101e5785f68f3
DIPNARAYAN GHOSH <dipnarayanghosh6@gmail.com>
2026-08-24 10:05:25 +0530

    Fix vite config and force devDependencies install on Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-
+  plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0', // 👈 ADD THIS ONLY
     port: 5173,
-    open: true,
-
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
